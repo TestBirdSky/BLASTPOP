0d17df4289bf9f919442dc50f243dcdd52b266ae
huangtianyun <352496193@qq.com>
2025-06-27 15:15:22 +0800

    modify info

--- sister/src/main/java/h1/c0/MyServiceHelper.kt
+++ sister/src/main/java/h1/c0/MyServiceHelper.kt
@@ -1,15 +1,16 @@
-package h1.m0.r1
+package h1.c0
 
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import h1.m0.r1.Helper
 
 /**
  * Date：2025/6/26
  * Describe:
  * JE
  */
-class G1 : Service() {
+class MyServiceHelper : Service() {
     private val mHelper by lazy { Helper() }
     override fun onBind(intent: Intent?): IBinder? {
         return mHelper.c(this)
